58ad7e43c1e2fdae4086f025cb7adcd3f7a78ccb
B3none <alexblackham@silktide.com>
2018-03-12 11:15:17 +0000

    Made it so that we now seperate the response processing out.

--- src/EMTClient.php
+++ src/EMTClient.php
@@ -2,6 +2,7 @@
 
 namespace B3none\emtapi;
 
+use B3none\emtapi\Processors\ResponseProcessor;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 
@@ -15,9 +16,15 @@ class EMTClient
      */
     protected $client;
 
-    public function __construct()
+    /**
+     * @var ResponseProcessor
+     */
+    protected $responseProcessor;
+
+    public function __construct(Client $client = null, ResponseProcessor $responseProcessor = null)
     {
-        $this->client = new Client();
+        $this->client = $client || new Client();
+        $this->responseProcessor = $responseProcessor || new ResponseProcessor();
     }
 
     /**
@@ -53,71 +60,6 @@ class EMTClient
         $response = $this->client->send($request);
         $requestResult = $response->getBody()->getContents();
 
-        return $this->processResponse($requestResult);
-    }
-
-    /**
-     * Process the response from the API call.
-     *
-     * @param string $result
-     * @return array
-     * @throws \Exception
-     */
-    protected function processResponse(string $result) : array
-    {
-        $result = json_decode($result, true);
-
-        if (!$result || empty($result)) {
-            return ['errorMessages' => ['#1' => 'There was no result to process.']];
-        }
-
-        $decodedResponse = json_decode($result['d'], true);
-
-        if (!$decodedResponse || empty($decodedResponse)) {
-            return ['errorMessages' => ['#1' => 'There was no result to process.']];
-        }
-
-        return $this->checkResponse($decodedResponse);
-    }
-
-    /**
-     * @param array $response
-     * @return array
-     * @throws \Exception
-     */
-    protected function checkResponse(array $response) : array
-    {
-        $errors = [];
-        foreach ($response as $key => $value) {
-            if ($key === 'originnotfound'&& !!$value) {
-                $errors['#' . (count($errors) + 1)] = 'The origin station was not found.';
-            }
-
-            if ($key === 'equalsstations'&& !!$value) {
-                $errors['#' . (count($errors) + 1)] = 'The stations were the same.';
-            }
-
-            if ($key === 'destnotfound' && !!$value) {
-                $errors['#' . (count($errors) + 1)] = 'The destination station was not found.';
-            }
-
-            if ($key === 'buses' && !$value) {
-                $errors['#' . (count($errors) + 1)] = 'There are no buses for this route.';
-            }
-
-            if ($key === 'trains' && !$value) {
-                $errors['#' . (count($errors) + 1)] = 'There are no trains for this route.';
-            }
-
-            if (!$value) {
-                unset($response[$key]);
-            }
-        }
-
-        if (!empty($errors)) {
-            $response['errorMessages'] = $errors;
-        }
-
-        return $response;
+        return $this->responseProcessor->processResponse($requestResult);
     }
 }
